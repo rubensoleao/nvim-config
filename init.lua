5a27f67eb6c9011678f8cece713322502cf26ccf
Rubens O Leão <rubensoleao@gmail.com>
2025-09-17 11:43:03 -0400

    feat: bootstrap lazy.vim

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
+
+-- Bootstrap lazy.nvim (cross-platform)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- Load lazy.nvim
 vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
