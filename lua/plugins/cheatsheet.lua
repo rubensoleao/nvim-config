c8d6904e0c8afc7dbb7dad78354fb2d08634a563
Rubens O Leao <rubensoleao@gmail.com>
2025-09-30 01:15:04 -0400

    Use my fork of cheatsheet

--- lua/plugins/cheatsheet.lua
+++ lua/plugins/cheatsheet.lua
@@ -1,21 +1,6 @@
--- 1) Custom cheatsheet items
-local custom = {
-  { key = "<leader>ff", desc = "Find files (Telescope)", mode = "n" },
-  { key = "<leader>fg", desc = "Live grep (Telescope)",  mode = "n" },
-  { key = "<leader>fb", desc = "List buffers (Telescope)", mode = "n" },
-  { key = "<leader>fh", desc = "Help tags (Telescope)",  mode = "n" },
-
-  { key = "<leader>r",  desc = "Open Ranger",            mode = "n" },
-
-  -- marks/jumps helpers (adjust if you remapped)
-  { key = "ma",         desc = "Set mark a",             mode = "n" },
-  { key = "'a",         desc = "Jump to mark a (line)",  mode = "n" },
-  { key = "`a",         desc = "Jump to mark a (exact)", mode = "n" },
-}
-
+-- Custom cheatsheet items organized by category
 return {
-  "sudormrfbin/cheatsheet.nvim",
-
+  "rubensoleao/cheatsheet.nvim",
   dependencies = {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/popup.nvim",
@@ -25,10 +10,8 @@ return {
   config = function()
     require("cheatsheet").setup({
 
-      include_only_installed_plugins = true,
-
       bundled_cheatsheets = {
-        enabled = { "default", "unicode", "nerd-fonts" },
+        enabled = { "default", "custom"},
       },
       bundled_plugin_cheatsheets = {
         enabled = { "telescope"}, -- load plugin cheatsheets too
